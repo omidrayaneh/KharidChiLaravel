d0438308e6db96c880dcf88422851419725052cd
omidrayaneh <omidrayaneh@gmail.com>
2019-04-14 19:29:38 +0430

    fix BUGGGGGgit add .!

--- resources/views/frontend/cart/index.blade.php
+++ resources/views/frontend/cart/index.blade.php
@@ -1,11 +1,24 @@
 @extends('frontend.layout.master')
+
+
 @section('content')
-    @if(Session::has('error'))
-        <div class="alert alert-warning">
-            <div>{{session('error')}}</div>
-        </div>
-    @endif
+
     <div class="container">
+        @if(Session::has('error'))
+            <div class="alert alert-warning">
+                <div>{{session('error')}}</div>
+            </div>
+        @endif
+        @if(Session::has('warning'))
+            <div class="alert alert-warning">
+                <div>{{session('warning')}}</div>
+            </div>
+        @endif
+        @if(Session::has('success'))
+            <div class="alert alert-success">
+                <div>{{session('success')}}</div>
+            </div>
+        @endif
         <!-- Breadcrumb Start-->
         <ul class="breadcrumb">
             <li><a href="index.html"><i class="fa fa-home"></i></a></li>
@@ -20,7 +33,7 @@
                     <table class="table table-bordered">
                         <thead>
                         <tr>
-                            <td class="text-center" width="10%">تصویر</td>
+                            <td class="text-center" width="9%">تصویر</td>
                             <td class="text-left">نام محصول</td>
                             <td class="text-left">کد محصول</td>
                             <td class="text-left">تعداد</td>
@@ -29,32 +42,27 @@
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach($cart->items as $product)
-                            <tr>
-                                <td width="10%" class="text-center"><a href="#"><img
-                                                src="{{$product['item']->photos[0]->path}}"
-                                                class="img-thumbnail"/></a></td>
-                                <td class="text-left"><a href="#">{{$product['item']->title}}</a><br/>
-                                <td class="text-left">{{$product['item']->sku}}</td>
-                                <td class="text-left">
-                                    <div class="input-group btn-block quantity text-center" >
+                            @foreach($cart->items as $product)
+                                <tr>
+                                    <td class="text-center" width="9%"><a href="#"><img src="{{$product['item']->photos[0]->path}}" class="img-thumbnail" /></a></td>
+                                    <td class="text-left"><a href="#">{{$product['item']->title}}</a><br />
+                                    <td class="text-left">{{$product['item']->sku}}</td>
+                                    <td class="text-left">
+                                        <div class="input-group btn-block quantity text-center">
+                                            <a class="btn btn-primary" data-toggle="tooltip" title="اضافه"  href="{{route('cart.add', ['id' => $product['item']->id])}}"><i class="fa fa-plus"></i></a>
+                                            <button type="button" data-toggle="tooltip" title="کم" class="btn btn-danger" onclick="event.preventDefault();
+                                                    document.getElementById('remove-cart-item_{{$product['item']->id}}').submit();"><i class="fa fa-minus"></i></button>
+                                            <form id="remove-cart-item_{{$product['item']->id}}" action="{{ route('cart.remove', ['id' => $product['item']->id]) }}" method="post" style="display: none;">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                        <p class="text-center" style="margin-top: 10px;">تعداد:‌ <span>{{$product['qty']}}</span></p>
 
-                                        <span class="input-group-btn">
-                                            <a  class="btn btn-primary" data-toggle="tooltip" title="افزودن" href="{{route('cart.add',['id'=>$product['item']->id])}}"><i class="fa fa-plus"></i></a>
-                                            <button type="button" data-toggle="tooltip" title="حذف" class="btn btn-danger"
-                                                    onclick="event.preventDefault();document.getElementById('remove-cart-item_{{$product['item']->id}}').submit();">
-                                                <i class="fa fa-minus"></i></button>
-                                        </span>
-                                        <form id="remove-cart-item_{{$product['item']->id}}" action="{{ route('cart.remove',['id' => $product['item']->id]) }}" method="post" style="display: none;">
-                                            @csrf
-                                        </form>
-                                        <p>تعداد : <span >{{$product['qty']}}</span></p>
-                                    </div>
-                                </td>
-                                <td class="text-right">{{$product['item']->discount_price ? $product['item']->discount_price : $product['item']->price}}  تومان </td>
-                                <td class="text-right">{{$product['price']}} تومان </td>
-                            </tr>
-                        @endforeach
+                                    </td>
+                                    <td class="text-right">{{$product['item']->discount_price ? $product['item']->discount_price : $product['item']->price }} تومان</td>
+                                    <td class="text-right">{{$product['price']}} تومان</td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -68,22 +76,39 @@
                             </div>
                             <div id="collapse-coupon" class="panel-collapse collapse in">
                                 <div class="panel-body">
-                                    <label class="col-sm-4 control-label" for="input-coupon">کد تخفیف خود را در اینجا
-                                        وارد کنید</label>
-                                    <form action="{{ route('coupon.add') }}" method="post" >
-                                        @csrf
+                                    <label class="col-sm-4 control-label" for="input-coupon">کد تخفیف خود را در اینجا وارد کنید</label>
+                                    <form action="{{ route('coupon.add') }}" method="post">
+                                    @csrf
                                         <div class="input-group">
-                                            <input type="text" name="code" placeholder="کد تخفیف خود را در اینجا وارد کنید" class="form-control"/>
+                                            <input type="text" name="code" placeholder="کد تخفیف خود را در اینجا وارد کنید" class="form-control" />
                                             <span class="input-group-btn">
-                                                  <button type="submit" class="btn btn-primary"
-                                                         id="button-coupon">اعمال کوپن</button>
-                                             </span>
+                                                <button type="submit"
+                                                        class="btn btn-primary" >اعمال کوپن</button>
+                                            </span>
                                         </div>
                                     </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    {{--<div class="col-sm-6">--}}
+                        {{--<div class="panel panel-default">--}}
+                            {{--<div class="panel-heading">--}}
+                                {{--<h4 class="panel-title">استفاده از کارت هدیه</h4>--}}
+                            {{--</div>--}}
+                            {{--<div id="collapse-voucher" class="panel-collapse collapse in">--}}
+                                {{--<div class="panel-body">--}}
+                                    {{--<label class="col-sm-4 control-label" for="input-voucher">کد کارت هدیه را در اینجا وارد کنید</label>--}}
+                                    {{--<div class="input-group">--}}
+                                        {{--<input type="text" name="voucher" value="" placeholder="کد کارت هدیه را در اینجا وارد کنید" id="input-voucher" class="form-control" />--}}
+                                        {{--<span class="input-group-btn">--}}
+                      {{--<input type="submit" value="اعمال کارت هدیه" id="button-voucher" data-loading-text="بارگذاری ..."  class="btn btn-primary" />--}}
+                      {{--</span> </div>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
                 </div>
                 <div class="row">
                     <div class="col-sm-4 col-sm-offset-8">
@@ -91,29 +116,21 @@
                             <tbody>
                             <tr>
                                 <td class="text-right"><strong>جمع کل</strong></td>
-                                <td class="text-right">{{Session::get('cart')->totalPurePrice}}
-                                    تومان
-                                </td>
+                                <td class="text-right">{{Session::get('cart')->totalPurePrice}} تومان</td>
                             </tr>
                             <tr>
                                 <td class="text-right"><strong>تخفیف</strong></td>
-                                <td class="text-right">{{Session::get('cart')->totalDiscountPrice}}
-                                    تومان
-                                </td>
+                                <td class="text-right">{{Session::get('cart')->totalDiscountPrice}} تومان</td>
                             </tr>
                             @if(Auth::check() && Session::get('cart')->coupon)
                                 <tr>
                                     <td class="text-right"><strong>{{Session::get('cart')->coupon['coupon']->title}}</strong></td>
-                                    <td class="text-right">{{Session::get('cart')->couponDiscount}} تومان </td>
+                                    <td class="text-right">{{Session::get('cart')->couponDiscount}} تومان</td>
                                 </tr>
                             @endif
-                            {{-- <tr>
-                                 <td class="text-right"><strong>مالیات</strong></td>
-                                 <td class="text-right">11880 تومان</td>
-                             </tr>--}}
                             <tr>
                                 <td class="text-right"><strong>قابل پرداخت</strong></td>
-                                <td class="text-right">{{Session::get('cart')->totalPrice}} تومان </td>
+                                <td class="text-right">{{Session::get('cart')->totalPrice}} تومان</td>
                             </tr>
                             </tbody>
                         </table>
@@ -121,10 +138,11 @@
                 </div>
                 <div class="buttons">
                     <div class="pull-left"><a href="{{url('/')}}" class="btn btn-default">ادامه خرید</a></div>
-                    <div class="pull-right"><a href="checkout.html" class="btn btn-primary">تسویه حساب</a></div>
+                    <div class="pull-right"><a href="" class="btn btn-primary">تسویه حساب</a></div>
                 </div>
             </div>
             <!--Middle Part End -->
         </div>
     </div>
+
 @endsection
--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -32,9 +32,9 @@
                     <div class="owl-carousel product_carousel_tab">
                         @foreach($latestProduct as $product)
                             <div class="product-thumb clearfix">
-                                <div class="image"><a href="product.html"><img src="{{$product->photos[0]->path}}" alt="تی شرت کتان مردانه" title="تی شرت کتان مردانه" class="img-responsive" /></a></div>
+                                <div class="image"><a href="{{route('product.single', ['slug'=> $product->slug])}}"><img src="{{$product->photos[0]->path}}" alt="تی شرت کتان مردانه" title="تی شرت کتان مردانه" class="img-responsive" /></a></div>
                                 <div class="caption">
-                                    <h4><a href="product.html">{{$product->title}}</a></h4>
+                                    <h4><a href="{{route('product.single', ['slug'=> $product->slug])}}">{{$product->title}}</a></h4>
                                     @if($product->discount_price)
                                         <p class="price"><span class="price-new">{{$product->discount_price}} تومان</span> <span class="price-old">{{$product->price}} تومان </span><span class="saving">{{round((($product->price-$product->discount_price)*100)/$product->price)}}%</span></p>
                                     @else
